69d3a0f0a678c7a93a92a021a40bb1b80dc93904
AlperenEkinci06 <work.alperenekinci@gmail.com>
2024-07-23 02:04:25 +0300

    Voxel Updated

--- Assets/Scripts/VoxelGenerator/VoxelManipulator.cs
+++ Assets/Scripts/VoxelGenerator/VoxelManipulator.cs
@@ -7,6 +7,7 @@ public class VoxelManipulator : MonoBehaviour
     [Header("Voxel Manipulator Communication")]
     [SerializeField] CustomInputManager customInputManager;
     [Header("Voxel Manipulator Settings")]
+    [SerializeField] Transform indicatorTransform;
     [SerializeField][Range(3, 8)] int brushSize = 3;
     RaycastHit hit;
 
@@ -15,83 +16,88 @@ public class VoxelManipulator : MonoBehaviour
         StartCoroutine(InputCheck());
     }
 
+    private void Update()
+    {
+        RaycastMouse();
+        PlaceIndicator();
+    }
+
+    void PlaceIndicator()
+    {
+        if (hit.transform)
+        {
+            indicatorTransform.gameObject.SetActive(true);
+            indicatorTransform.position = hit.point;
+            indicatorTransform.localScale = Vector3.one * brushSize;
+        }
+        else
+        {
+            indicatorTransform.gameObject.SetActive(false);
+        }
+    }
+
+    void RaycastMouse()
+    {
+        hit = new RaycastHit();
+
+        LayerMask layerMask = LayerMask.GetMask("Terrain");
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(mouseRay, out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Ignore);
+    }
+
     IEnumerator InputCheck()
     {
         while (true)
         {
             if (customInputManager.GetCustomInputValue("Modify") > 0.9f)
             {
-                LayerMask layerMask = LayerMask.GetMask("Terrain");
-
-                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(mouseRay, out hit, 96, layerMask, QueryTriggerInteraction.Ignore))
+                if (hit.transform)
                 {
                     if (hit.transform.parent.CompareTag("Terrain"))
                     {
-                        MapGenerator mapGenerator = hit.transform.parent.GetComponent<MapGenerator>();
-                        MapDisplay mapDisplay = hit.transform.parent.GetComponent<MapDisplay>();
-
-                        int CenterChunkNum = int.Parse(hit.collider.name.Split(' ')[1]);
-                        int[] chunkArray = new int[brushSize * brushSize];
-
-
-                        for (int i = 0; i < brushSize * brushSize; i++)
-                        {
-                            chunkArray[i] = CenterChunkNum - (mapGenerator.densityMapWidth / mapGenerator.chunkSize + 1) + (i / 3) * (mapGenerator.densityMapWidth / mapGenerator.chunkSize) + i % 3;
-                        }
-                        for (int i = 0; i < brushSize * brushSize; i++)
-                        {
-                            Transform chunkT = null;
-                            try
-                            {
-                                chunkT = mapGenerator.chunkTransforms[chunkArray[i]];
-                            }
-                            catch
-                            {
-                                continue;
-                            }
-                            mapDisplay.DrawVoxel(chunkT, VoxelGenerator.GenerateTerrainVoxel(VoxelGenerator.ManipulateVoxelAtPosition(hit, mapGenerator.noiseMap3D, mapGenerator.voxelObjectSize, mapGenerator.detailMultiplier, brushSize, 0.0f), mapGenerator.voxelObjectSize / mapGenerator.detailMultiplier, mapGenerator.voxelThreshold, mapGenerator.chunkSize * mapGenerator.detailMultiplier, chunkArray[i]));
-                        }
+                        Draw3D(0.0f);
                     }
                 }
             }
             else if (customInputManager.GetCustomInputValue("Interact") > 0.9f)
             {
-                LayerMask layerMask = LayerMask.GetMask("Terrain");
-
-                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(mouseRay, out hit, 96, layerMask, QueryTriggerInteraction.Ignore))
+                if (hit.transform)
                 {
                     if (hit.transform.parent.CompareTag("Terrain"))
                     {
-                        MapGenerator mapGenerator = hit.transform.parent.GetComponent<MapGenerator>();
-                        MapDisplay mapDisplay = hit.transform.parent.GetComponent<MapDisplay>();
-
-                        int CenterChunkNum = int.Parse(hit.collider.name.Split(' ')[1]);
-                        int[] chunkArray = new int[brushSize * brushSize];
-
-
-                        for (int i = 0; i < brushSize * brushSize; i++)
-                        {
-                            chunkArray[i] = CenterChunkNum - (mapGenerator.densityMapWidth / mapGenerator.chunkSize + 1) + (i / 3) * (mapGenerator.densityMapWidth / mapGenerator.chunkSize) + i % 3;
-                        }
-                        for (int i = 0; i < brushSize * brushSize; i++)
-                        {
-                            Transform chunkT = null;
-                            try
-                            {
-                                chunkT = mapGenerator.chunkTransforms[chunkArray[i]];
-                            }
-                            catch
-                            {
-                                continue;
-                            }
-                            mapDisplay.DrawVoxel(chunkT, VoxelGenerator.GenerateTerrainVoxel(VoxelGenerator.ManipulateVoxelAtPosition(hit, mapGenerator.noiseMap3D, mapGenerator.voxelObjectSize, mapGenerator.detailMultiplier, brushSize, 1.0f), mapGenerator.voxelObjectSize / mapGenerator.detailMultiplier, mapGenerator.voxelThreshold, mapGenerator.chunkSize * mapGenerator.detailMultiplier, chunkArray[i]));
-                        }
+                        Draw3D(1.0f);
                     }
                 }
             }
             yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void Draw3D(float multiplier)
+    {
+        MapGenerator mapGenerator = hit.transform.parent.GetComponent<MapGenerator>();
+        MapDisplay mapDisplay = hit.transform.parent.GetComponent<MapDisplay>();
+
+        int CenterChunkNum = int.Parse(hit.collider.name.Split(' ')[1]);
+        int[] chunkArray = new int[brushSize * brushSize];
+
+
+        for (int i = 0; i < brushSize * brushSize; i++)
+        {
+            chunkArray[i] = CenterChunkNum - (mapGenerator.densityMapWidth / mapGenerator.chunkSize + 1) + (i / 3) * (mapGenerator.densityMapWidth / mapGenerator.chunkSize) + i % 3;
+        }
+        for (int i = 0; i < brushSize * brushSize; i++)
+        {
+            Transform chunkT = null;
+            try
+            {
+                chunkT = mapGenerator.chunkTransforms[chunkArray[i]];
+            }
+            catch
+            {
+                continue;
+            }
+            mapDisplay.DrawVoxel(chunkT, VoxelGenerator.GenerateTerrainVoxel(VoxelGenerator.ManipulateVoxelAtPosition(hit, mapGenerator.noiseMap3D, mapGenerator.voxelObjectSize, mapGenerator.detailMultiplier, brushSize, multiplier), mapGenerator.voxelObjectSize / mapGenerator.detailMultiplier, mapGenerator.voxelThreshold, mapGenerator.chunkSize * mapGenerator.detailMultiplier, chunkArray[i]));
         }
     }
 }
